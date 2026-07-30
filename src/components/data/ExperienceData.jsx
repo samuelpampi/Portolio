bceb7dc621032b5abb792e2bed110bea176495d5
samuelpampi <samuelpampillonroa@gmail.com>
2026-07-30 23:00:42 +0200

    add new experience in deloitte and tfm project

--- src/components/data/ExperienceData.jsx
+++ src/components/data/ExperienceData.jsx
@@ -1,9 +1,17 @@
 export const JOBS = [
     {
+        title: "Delivery Consultant",
+        company: "Deloitte",
+        time: "Apr 2026 - Actualidad",
+        description: "Desarrollo de soluciones de software para clientes, desde la implementación de funcionalidades, hasta el mantemiento y procesos de despliegue y soporte técnico. Creada una aplicación GRC específica para un cliente lider en el sector de las telecomunicaciones.",
+        techs: ["python", "fastapi", "react", "typescript", "next", "sqlserver", "docker"]
+    },
+    {
         title: "Industrial Cybersecurity Developer",
         company: "InprOTech",
-        time: "Mar 2024 - Actualidad",
+        time: "Mar 2024 - Apr 2026",
         description: "Desarrollo del software de monitorización de ciberseguridad industrial InprOTech Guardian, desde la implementación de funcionalidades, hasta el mantemiento y procesos de despliegue y soporte técnico. Implementado +10 funcionalidades clave, optimizado hasta un 50% de procesos de despliege. Publicado +10 artículos técnicos y +2 charlas en centros educativos.",
         techs: ["python", "angular", "typescript", "kafka", "postgresql", "docker"]
     }
+    
 ];
